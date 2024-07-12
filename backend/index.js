8620f8d7de8f9b438c7b351584ae2bec26b168be
t-whelan <whelan.tionne@gmail.com>
2024-07-12 10:36:31 +0200

    backend update

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,18 @@ app.use(
     allowedHeaders: ["Content-Type"],
   })
 );
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mern-stack-bookstore-2.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(express.json()); //formats jason data
 app.use("/books", booksRoute);
